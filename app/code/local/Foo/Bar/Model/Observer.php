7da8b89fb5fed4ee3e14bb0c4c53d50df2d64723
Daniel Herrera Cubells <daniel.herreracubells@altran.com>
2014-11-25 10:28:25 +0100

    VideoTutorial 2.8 Exercise Event - Observer

---
+++ app/code/local/Foo/Bar/Model/Observer.php
@@ -0,0 +1,12 @@
+<?php
+
+class Foo_Bar_Model_Observer
+{
+    public function catalogProductLoadAfter (Varien_Event_Observer $observer)
+    {
+        $product = $observer->getProduct();
+        $product ->setName($product->getName().' is cool!!');
+    }
+    
+}
+
