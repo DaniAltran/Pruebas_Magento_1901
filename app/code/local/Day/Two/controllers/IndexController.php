f0a7f92109d688e689d11232b666e94c43af0e2d
Daniel Herrera Cubells <daniel.herreracubells@altran.com>
2014-11-26 13:23:30 +0100

    3.6 Exercise Design and Layout.

--- app/code/local/Day/Two/controllers/IndexController.php
+++ app/code/local/Day/Two/controllers/IndexController.php
@@ -12,5 +12,29 @@ class Day_Two_IndexController extends Mage_Core_Controller_Front_Action
     {
         echo get_class(Mage::getModel('day_two/whatever'));
     }
+    
+    
+    //http://localhost/magento_1901/heyo/index/layout
+    
+    public function layoutAction()
+    {
+        $xml = $this->loadLayout()->getLayout()->getUpdate()->asString();
+        
+        $this->getResponse()->setHeader('Content-Type','text/plain')->setBody($xml);
+        
+        //Mage::log($xml,Zend_log::INFO,'layout_log',true);
+        
+        $model = Mage::getModel('day_two/whatever');
+        //Mage::log($model,Zend_log::INFO,'model_log',true);
+        
+        Mage::log($model->debug(),Zend_log::INFO,'model_log',true);
+    }
+    
+    //http://localhost/magento_1901/heyo/index/default
+    
+    public function defaultAction()
+    {
+        $this->loadLayout()->renderLayout();
+    }
 }
 
