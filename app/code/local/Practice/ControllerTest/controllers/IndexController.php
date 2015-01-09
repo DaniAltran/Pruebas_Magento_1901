e62eb9b65c8e562c4ecad71f1fec030ffa76b94e
unknown <daniel.herrera@08APC0504.altran.es>
2015-01-09 13:38:47 +0100

    YOUTUBE - New VIDEOTUTORIAL. Quick Start to Magento Customization Part 2 - 33:00 time

---
+++ app/code/local/Practice/ControllerTest/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Practice_ControllerTest_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo '<h1>Hello Dani</h1>';
+    }
+}
+
